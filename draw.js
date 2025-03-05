1556b0b7a343dfcf94857227ae1c8a485979f890
Ziyiiiii0718 <sproutedpotato0718@gmail.com>
2025-03-05 12:38:53 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,45 +1,113 @@
 window.addEventListener("load", function () {
-    let c = document.getElementById("color");
-    let s = document.getElementById("shape");
-    let ca = document.getElementById("canvas1");
-    let r = document.getElementById("range");
-    r.addEventListener("input", function (event) {
+    let colorInput = document.getElementById("color");
+    let shapeSelect = document.getElementById("shape");
+    let canvas = document.getElementById("canvas1");
+    let sizeInput = document.getElementById("range");
+    let undoButton = document.getElementById("undo");
+    let clearButton = document.getElementById("clear");
+    let ctx = canvas.getContext("2d");
+    
+    let shapesArray = [];
+
+    class Shape {
+        constructor(x, y, color, size) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+            this.size = size;
+        }
+        draw(ctx) {}
+    }
+
+    class Rectangle extends Shape {
+        draw(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.size + 10, this.size);
+        }
+    }
+
+    class Circle extends Shape {
+        draw(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
+    class Triangle extends Shape {
+        draw(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x - this.size / 2, this.y + this.size);
+            ctx.lineTo(this.x + this.size / 2, this.y + this.size);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+    sizeInput.addEventListener("input", function (event) {
         let v = parseInt(this.value);
         v = v - 0;
         let msg = document.getElementById("rangeamount");
         msg.innerHTML = v;
     });
-    document.getElementById("clear").addEventListener("click", function (event) {
-        ctx.clearRect(0, 0, ca.width, ca.height);
-    });
-    function whereClicked(event) {
-        var x = event.pageX - this.offsetLeft;
-        var y = event.pageY - this.offsetTop;
 
-        var ctx = this.getContext("2d");
-        ctx.fillStyle = c.value;
-
-        ca.addEventListener("click", function (event) {
-            console.log(event.pageX - this.offsetLeft, event.pageY - this.offsetTop);
-            let x = event.pageX - this.offsetLeft;
-            let y = event.pageY - this.offsetTop;
-
-            //Rectangle
-            ctx.fillRect(x - 10, y - 10, parseInt(r.value),parseInt(r.value)+10);
-
-            //Circle
-            //ctx.beginPath();
-            //ctx.arc(x - 10, y - 10, parseInt(r.value), 0, Math.PI * 2);
-            //ctx.fill();
-
-            //triangle
-
-
-
-        });
-
-
+    function redrawCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let shape of shapesArray) {
+            shape.draw(ctx);
+        }
     }
 
-    document.getElementById("canvas1").addEventListener("mousemove", whereClicked);
+    function addShape(event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        let color = colorInput.value;
+        let size = parseInt(sizeInput.value);
+        let shapeType = shapeSelect.value;
+        let shape;
+
+        if (shapeType === "rectangle") {
+            shape = new Rectangle(x, y, color, size);
+        } else if (shapeType === "circle") {
+            shape = new Circle(x, y, color, size);
+        } else if (shapeType === "triangle") {
+            shape = new Triangle(x, y, color, size);
+        }
+
+        shapesArray.push(shape);
+        shape.draw(ctx);
+        saveToLocalStorage();
+    }
+
+    function undoLastShape() {
+        shapesArray.pop();
+        redrawCanvas();
+        saveToLocalStorage();
+    }
+
+    function clearCanvas() {
+        shapesArray = [];
+        redrawCanvas();
+        localStorage.removeItem("shapesData");
+    }
+
+    function saveToLocalStorage() {
+        localStorage.setItem("shapesData", JSON.stringify(shapesArray));
+    }
+
+    function loadFromLocalStorage() {
+        let savedShapes = localStorage.getItem("shapesData");
+        if (savedShapes) {
+            shapesArray = JSON.parse(savedShapes);
+            redrawCanvas();
+        }
+    }
+    
+    canvas.addEventListener("click", addShape);
+    undoButton.addEventListener("click", undoLastShape);
+    clearButton.addEventListener("click", clearCanvas);
+
+    loadFromLocalStorage();
 });
